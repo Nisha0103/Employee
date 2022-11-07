e2086608d65ddb49558194041ca109d019b5ed72
nisha0103 <natrajnisha@gmail.com>
2022-11-07 21:02:10 +0530

    message

---
+++ src/test/java/org/tcs/TrialGit.java
@@ -0,0 +1,23 @@
+package org.tcs;
+
+public class TrialGit {
+	
+	public void test() {
+		System.out.println("test");
+
+		
+
+}
+	
+	public void test2() {
+		System.out.println("test");
+		
+	}
+	
+	public void test3() {
+		System.out.println("test2");
+
+	}
+}
+	
+	
